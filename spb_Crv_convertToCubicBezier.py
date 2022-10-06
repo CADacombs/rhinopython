d985fe54e2a70b61b750361af185460d5015b32e
sburzinski <spburzin@gmail.com>
2022-10-06 17:23:32 -0500

    Add files via upload

--- spb_Crv_convertToCubicBezier.py
+++ spb_Crv_convertToCubicBezier.py
@@ -1,11 +1,11 @@
 """
-End conditions will automatically be tangency-matched to input curve.
+End conditions must be G1-matched to input curve and/or reference curve(s).
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 """
-221005: Created.
+221005-06: Created.
 """
 
 import Rhino
@@ -95,7 +95,7 @@ class Opts():
         sc.sticky[cls.stickyKeys[key]] = cls.values[key]
 
 
-def getInput():
+def getInput_DistDevRef():
     """
     Get curve with optional input.
     """
@@ -105,21 +105,6 @@ def getInput():
     go.SetCommandPrompt("Select reference curve")
 
     go.GeometryFilter = Rhino.DocObjects.ObjectType.Curve
-
-    def customGeometryFilter(rdObj, rgObj, compIdx):
-        if isinstance(rgObj, rg.BrepEdge):
-            crv = rgObj.DuplicateCurve()
-        else:
-            crv = rgObj
-        if not isinstance(crv, rg.NurbsCurve):
-            return True
-        nc = crv
-        if nc.Degree == 3 and nc.SpanCount == 1 and not nc.IsRational:
-            #print("Curve is already a non-rational, cubic Bezier.")
-            return False
-        return True
-    go.SetCustomGeometryFilter(customGeometryFilter)
-
 
     idxs_Opt = {}
 
@@ -143,6 +128,24 @@ def getInput():
 
         if res == ri.GetResult.Object:
             objref = go.Object(0)
+            crv = objref.Curve()
+            if isinstance(crv, rg.BrepEdge):
+                crv = crv.DuplicateCurve()
+
+            if crv.IsClosed:
+                print("Closed curves are not supported.")
+                sc.doc.Objects.UnselectAll()
+                go.ClearObjects()
+                sc.doc.Views.Redraw()
+                continue
+
+            if isinstance(crv, rg.NurbsCurve) and crv.Degree == 3 and crv.SpanCount == 1 and not crv.IsRational:
+                print("Curve is already a non-rational, cubic Bezier.")
+                sc.doc.Objects.UnselectAll()
+                go.ClearObjects()
+                sc.doc.Views.Redraw()
+                continue
+
             go.Dispose()
             return objref
 
@@ -150,6 +153,113 @@ def getInput():
             if go.Option().Index == idxs_Opt[key]:
                 Opts.setValue(key, go.Option().CurrentListOptionIndex)
                 break
+
+
+def getInput_Tan():
+    """
+    Get 2 curves with optional input.
+    """
+    
+    go = ri.Custom.GetObject()
+
+    go.SetCommandPrompt("Select 1 or 2 curves near end to acquire G1")
+
+    go.GeometryFilter = Rhino.DocObjects.ObjectType.Curve
+
+    go.AcceptNothing(True)
+    go.DisablePreSelect()
+    go.OneByOnePostSelect = True
+
+    idxs_Opts = {}
+
+    def addOption(key): idxs_Opts[key] = Opts.addOption(go, key)
+
+    while True:
+        go.ClearCommandOptions()
+
+        idxs_Opts.clear()
+
+        addOption('bReplace')
+        addOption('bEcho')
+        addOption('bDebug')
+
+        res = go.GetMultiple(minimumNumber=1, maximumNumber=2)
+
+        if res == ri.GetResult.Cancel:
+            go.Dispose()
+            return
+
+        if res == ri.GetResult.Nothing:
+            go.Dispose()
+            return []
+
+        if res == ri.GetResult.Object:
+            objrefs = go.Objects()
+            go.Dispose()
+            return objrefs
+
+        for key in idxs_Opts:
+            if go.Option().Index == idxs_Opts[key]:
+                Opts.setValue(key, go.Option().CurrentListOptionIndex)
+                break
+
+
+def getDataForEndCondition(objrefs_MatchTanCrvs):
+    """
+    Returns:
+        tuple(rg.Point3d or None), tuple(rg.Vector3d or None)
+        Vectors are going toward their curves.
+    """
+
+    pts = []
+    tans = []
+
+    for objref in objrefs_MatchTanCrvs:
+
+        rgC = objref.Curve()
+
+        bSuccess, t = rgC.ClosestPoint(objref.SelectionPoint())
+
+        if t < rgC.Domain.Mid:
+            # T0
+            pts.append(rgC.PointAtStart)
+            tans.append( rgC.TangentAtStart)
+        else:
+            # T1
+            pts.append(rgC.PointAtEnd)
+            tans.append(-rgC.TangentAtEnd)
+
+    return pts, tans
+
+
+def matchG1DataToDistDevCrv(objrefs_MatchTanCrvs, crv_In_DistDev):
+    """
+    Returns:
+        tuple(rg.Point3d or None), tuple(rg.Vector3d or None)
+    """
+
+    if len(objrefs_MatchTanCrvs) == 0:
+        return (None, None), (None, None)
+
+    rc = getDataForEndCondition(objrefs_MatchTanCrvs)
+    if rc is None: return
+
+    pts, tans = rc
+
+    dist_to_start = pts[0].DistanceTo(crv_In_DistDev.PointAtStart)
+    dist_to_end = pts[0].DistanceTo(crv_In_DistDev.PointAtEnd)
+
+    if len(pts) == 1:
+        if dist_to_start <= dist_to_end:
+            return (pts[0], None), (-tans[0], None)
+        else:
+            return (None, pts[0]), (None, tans[0])
+
+    if len(pts) == 2:
+        if dist_to_start <= dist_to_end:
+            return (pts[0], pts[1]), (-tans[0], tans[1])
+        else:
+            return (pts[1], pts[0]), (-tans[1], tans[0])
 
 
 def _formatDistance(fDistance):
@@ -169,10 +279,10 @@ def _getMaxDev(rgCrvA, rgCrvB):
         return rc[1]
 
 
-def createCurve(crv_In, **kwargs):
+def createCurveIfSpecialCase(crv_In, **kwargs):
     """
     returns:
-        Success: New NurbsCurve with deviation less than nc0
+        Success: New NurbsCurve
         Fail: None
     """
 
@@ -181,38 +291,41 @@ def createCurve(crv_In, **kwargs):
     bEcho = getOpt('bEcho')
     bDebug = getOpt('bDebug')
 
-    if isinstance(crv_In, rg.LineCurve):
-        nc_Out = crv_In.Rebuild(pointCount=4, degree=3, preserveTangents=False)
+
+    bEdge = isinstance(crv_In, rg.BrepEdge)
+
+    crv_NotEdge = crv_In.DuplicateCurve()
+
+    if isinstance(crv_NotEdge, rg.LineCurve):
+        nc_Out = crv_NotEdge.Rebuild(pointCount=4, degree=3, preserveTangents=False)
         if nc_Out:
-            print("Input is a LineCurve.")
-            return nc_Out
-        return
-    if crv_In.IsLinear(Rhino.RhinoMath.ZeroTolerance):
-        nc_Out = rg.LineCurve(crv_In.PointAtStart, crv_In.PointAtEnd).Rebuild(pointCount=4, degree=3, preserveTangents=False)
-        if nc_Out:
-            print("Input is a linear {}.".format(crv_In.GetType().Name))
+            if bEcho:
+                print("Input is a LineCurve.")
             return nc_Out
         return
 
-    if isinstance(crv_In, rg.NurbsCurve) and crv_In.Degree==2 and crv_In.SpanCount==1 and not crv_In.IsRational:
-        nc_Out = crv_In.DuplicateCurve()
+    if crv_NotEdge.IsLinear(Rhino.RhinoMath.ZeroTolerance):
+        nc_Out = rg.LineCurve(crv_NotEdge.PointAtStart, crv_NotEdge.PointAtEnd).Rebuild(pointCount=4, degree=3, preserveTangents=False)
+        if nc_Out:
+            if bEcho:
+                print("Input is a linear {}.".format(crv_NotEdge.GetType().Name))
+            return nc_Out
+        return
+
+    if isinstance(crv_NotEdge, rg.NurbsCurve) and crv_NotEdge.Degree==2 and crv_NotEdge.SpanCount==1 and not crv_NotEdge.IsRational:
+        nc_Out = crv_NotEdge.DuplicateCurve()
         if rg.NurbsCurve.IncreaseDegree(nc_Out, 3):
-            print("Input is a degree-2 NurbsCurve.")
+            if bEcho:
+                print("Input is a degree-2 NurbsCurve.")
             return nc_Out
         return
-
-    if isinstance(crv_In, rg.BrepEdge):
-        print("Input is a BrepEdge of a {}.".format(crv_In.DuplicateCurve().GetType().Name))
-    else:
-        print("Input is a {}.".format(crv_In.GetType().Name))
-
 
 
     arc = None
 
-    if isinstance(crv_In, rg.ArcCurve):
+    if isinstance(crv_NotEdge, rg.ArcCurve):
         arc = rg.ArcCurve.Arc
-    rc = rg.Curve.TryGetArc(crv_In, tolerance=Rhino.RhinoMath.ZeroTolerance)
+    rc = rg.Curve.TryGetArc(crv_NotEdge, tolerance=Rhino.RhinoMath.ZeroTolerance)
     if rc[0]:
         arc = rc[1]
 
@@ -235,16 +348,38 @@ def createCurve(crv_In, **kwargs):
 
         point3dList = createPoints_matchArcAtMidPoint(arc)
 
-        return rg.Curve.CreateControlPointCurve(point3dList, degree=3)
+        if point3dList is not None:
+            return rg.Curve.CreateControlPointCurve(point3dList, degree=3)
 
+
+def createCurve(crv_In, pts, tans, **kwargs):
+    """
+    returns:
+        Success: New NurbsCurve
+        Fail: None
+    """
+
+    def getOpt(key): return kwargs[key] if key in kwargs else Opts.values[key]
+
+    bEcho = getOpt('bEcho')
+    bDebug = getOpt('bDebug')
+
+    if isinstance(crv_In, rg.BrepEdge):
+        print("Input is a BrepEdge of a {}.".format(crv_In.DuplicateCurve().GetType().Name))
+    else:
+        print("Input is a {}.".format(crv_In.GetType().Name))
 
 
     fBulgeUnitDist = crv_In.PointAtStart.DistanceTo(crv_In.PointAtEnd)
 
-    pts = [crv_In.PointAtStart, None, None, crv_In.PointAtEnd]
+    pts = [
+        crv_In.PointAtStart if pts[0] is None else pts[0],
+        None,
+        None,
+        crv_In.PointAtEnd if pts[1] is None else pts[1]]
 
-    vA = crv_In.TangentAtStart # Already length of 1.0
-    vB = crv_In.TangentAtEnd # Already length of 1.0
+    vA = crv_In.TangentAtStart if tans[0] is None else tans[0] # Already length of 1.0
+    vB = crv_In.TangentAtEnd if tans[1] is None else tans[1] # Already length of 1.0
 
 
     def findCrv_Symmetrical():
@@ -273,6 +408,7 @@ def createCurve(crv_In, **kwargs):
             devs = []
 
             for iAB in range(int(1.0/fDivs) + 1):
+                sc.escape_test()
                 mAB = fmin * (1 - iAB * fDivs) + fmax * (iAB * fDivs)
                 if mAB == 0.0: continue
                 pts[1] = pts[0] + mAB * fBulgeUnitDist * vA
@@ -294,9 +430,10 @@ def createCurve(crv_In, **kwargs):
                 devs.append(dev)
 
             if not ncs_Res:
-                print("No curves were generated.")
                 fmin *= 10.0
                 fmax *= 10.0
+                print("No curves were generated."
+                    "Range of bulge multipliers increased to [{},{}]".format(fmin, fmax))
                 continue
 
 
@@ -358,10 +495,12 @@ def createCurve(crv_In, **kwargs):
             devs = []
 
             for iA in range(int(1.0/fDivs) + 1):
+                sc.escape_test()
                 mA = fmin_mA * (1 - iA * fDivs) + fmax_mA * (iA * fDivs)
                 if mA == 0.0: continue
                 pts[1] = pts[0] + mA * fBulgeUnitDist * vA
                 for iB in range(int(1.0/fDivs) + 1):
+                    sc.escape_test()
                     mB = fmin_mB * (1 - iB * fDivs) + fmax_mB * (iB * fDivs)
                     if mB == 0.0: continue
                     pts[2] = pts[3] - mB * fBulgeUnitDist * vB
@@ -463,14 +602,18 @@ def main():
     """
     """
 
-    objref_In = getInput()
-    if objref_In is None: return
+    objref_DistDevCrv = getInput_DistDevRef()
+    if objref_DistDevCrv is None: return
 
     bReplace = Opts.values['bReplace']
     bEcho = Opts.values['bEcho']
     bDebug = Opts.values['bDebug']
 
-    crv_In = objref_In.Curve()
+
+    objrefs_MatchTanCrvs = getInput_Tan()
+    if objrefs_MatchTanCrvs is None: return
+
+    crv_In_DistDev = objref_DistDevCrv.Curve()
 
 
     #if bDebug:
@@ -479,10 +622,20 @@ def main():
 
     Rhino.RhinoApp.CommandPrompt = "Working ..."
 
-    nc_Res = createCurve(
-        crv_In,
-        bEcho=bEcho,
-        bDebug=bDebug)
+    if objrefs_MatchTanCrvs:
+        nc_Res = None
+    else:
+        nc_Res = createCurveIfSpecialCase(crv_In_DistDev)
+
+
+    if nc_Res is None:
+        pts, tans = matchG1DataToDistDevCrv(objrefs_MatchTanCrvs, crv_In_DistDev)
+        nc_Res = createCurve(
+            crv_In_DistDev,
+            pts,
+            tans,
+            bEcho=bEcho,
+            bDebug=bDebug)
 
 
     if nc_Res is None:
@@ -490,7 +643,7 @@ def main():
         return
 
 
-    if objref_In.Edge() or not bReplace:
+    if objref_DistDevCrv.Edge() or not bReplace:
         gOut = sc.doc.Objects.AddCurve(nc_Res)
         if gOut != gOut.Empty:
             print("Curve was added.")
@@ -501,7 +654,7 @@ def main():
             return
 
 
-    if sc.doc.Objects.Replace(objref_In, curve=nc_Res):
+    if sc.doc.Objects.Replace(objref_DistDevCrv, curve=nc_Res):
         print("Curve was replaced.")
         sc.doc.Views.Redraw()
     else:

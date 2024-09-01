bc13dd242cf7c8f903fc63eceecf911bd4282753
sburzinski <spburzin@gmail.com>
2024-09-01 13:01:36 -0500

    Add files via upload

--- spb_Grips_linearize_per2Pts.py
+++ spb_Grips_linearize_per2Pts.py
@@ -5,15 +5,24 @@
 Options:
     DistributeEvenly=Yes: All control points will be evenly distributed between
         reference points.
+
+Send any questions, comments, or script development service needs to
+@spb on the McNeel Forums, https://discourse.mcneel.com/
 """
 
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+#! python 2
 
 """
 190904: Created.
 220824: Now multiple grips will be made inline to 2 reference grips.
-220922: Added bDistributeEvenly.  Refactored.
+220922: Added DistributeEvenly option.  Refactored.
 220923: Now, references are picked points, not other grips.
+240829-30: Added OuterGripsAtRefPts option for when DistributeEvenly and BetweenRefPts are enabled.
+
+TODO:
+    Allow points to be scaled so each of the 2 extents are on a reference point.
 """
 
 import Rhino
@@ -34,6 +43,16 @@ class Opts():
 
 
     key = 'bDistributeEvenly'; keys.append(key)
+    values[key] = False
+    riOpts[key] = ri.Custom.OptionToggle(values[key], 'No', 'Yes')
+    stickyKeys[key] = '{}({})'.format(key, __file__)
+
+    key = 'bBetweenRefPts'; keys.append(key)
+    values[key] = True
+    riOpts[key] = ri.Custom.OptionToggle(values[key], 'No', 'Yes')
+    stickyKeys[key] = '{}({})'.format(key, __file__)
+
+    key = 'bOuterGripsAtRefPts'; keys.append(key)
     values[key] = False
     riOpts[key] = ri.Custom.OptionToggle(values[key], 'No', 'Yes')
     stickyKeys[key] = '{}({})'.format(key, __file__)
@@ -105,38 +124,56 @@ def getInput_Grips_ToMove():
     
     #
     go = ri.Custom.GetObject()
-    go.SetCommandPrompt("Select grips/CPs to move")
+    go.SetCommandPrompt("Select grips to move")
     
     go.GeometryFilter = rd.ObjectType.Grip
     
+    # go.AlreadySelectedObjectSelect = True
+    go.DeselectAllBeforePostSelect = False
+    go.EnableClearObjectsOnEntry(False)
+    go.EnableUnselectObjectsOnExit(False)
+
     def addOption(key): idxs_Opt[key] = Opts.addOption(go, key)
     
     idxs_Opt = {}
-    
+
+    bFirstRun = True
+
     while True:
         go.ClearCommandOptions()
 
         idxs_Opt.clear()
 
         addOption('bDistributeEvenly')
+        if Opts.values['bDistributeEvenly']:
+            addOption('bBetweenRefPts')
+            if Opts.values['bBetweenRefPts']:
+                addOption('bOuterGripsAtRefPts')
         addOption('bEcho')
         addOption('bDebug')
 
+        sc.doc.Views.Redraw()
+
         res = go.GetMultiple(minimumNumber=1, maximumNumber=0)
-        
+
+        if res == ri.GetResult.Cancel:
+            go.Dispose()
+            return
+
         if res == ri.GetResult.Object:
             objrefs = go.Objects()
             go.Dispose()
             grips_ToMove = [o.Object() for o in objrefs]
             return grips_ToMove
-        elif res == ri.GetResult.Cancel:
-            go.Dispose()
-            return
 
         for key in idxs_Opt:
             if go.Option().Index == idxs_Opt[key]:
                 Opts.setValue(key, go.Option().CurrentListOptionIndex)
                 break
+
+        if bFirstRun:
+            go.EnablePreSelect(False, ignoreUnacceptablePreselectedObjects=True) # Default is True.
+            bFirstRun = False
 
 
 def getInput_Points_Ref():
@@ -148,7 +185,7 @@ def getInput_Points_Ref():
     for i, s in zip((0,1),('1st', '2nd')):
         gp = ri.Custom.GetPoint()
 
-        gp.SetCommandPrompt("Select {} reference point".format(s))
+        gp.SetCommandPrompt("Pick {} reference point".format(s))
     
 
         idxs_Opt = {}
@@ -159,6 +196,10 @@ def getInput_Points_Ref():
             idxs_Opt.clear()
 
             addOption('bDistributeEvenly')
+            if Opts.values['bDistributeEvenly']:
+                addOption('bBetweenRefPts')
+                if Opts.values['bBetweenRefPts']:
+                    addOption('bOuterGripsAtRefPts')
             addOption('bEcho')
             addOption('bDebug')
 
@@ -172,11 +213,6 @@ def getInput_Points_Ref():
                 pts[i] = gp.Point()
                 break
 
-            if res == ri.GetResult.Object:
-                objrefs = gp.Objects()
-                gp.Dispose()
-                grips_Ref = [objref.Object() for objref in objrefs]
-                return grips_Ref
             for key in idxs_Opt:
                 if gp.Option().Index == idxs_Opt[key]:
                     Opts.setValue(key, gp.Option().CurrentListOptionIndex)
@@ -185,7 +221,7 @@ def getInput_Points_Ref():
     return pts
 
 
-def processGrips(grips_ToMove, pts_Ref, bDistributeEvenly=False, bEcho=True, bDebug=False):
+def processGrips(grips_ToMove, pts_Ref, bDistributeEvenly=False, bBetweenRefPts=True, bOuterGripsAtRefPts=False, bEcho=True, bDebug=False):
     """
     """
 
@@ -196,7 +232,7 @@ def processGrips(grips_ToMove, pts_Ref, bDistributeEvenly=False, bEcho=True, bDe
     rdOwners = []
     gOwners = []
 
-    bSomePtTrans = False
+    bSomeGripsWereTranslated = False
 
     for grip_ToMove in grips_ToMove:
 
@@ -212,29 +248,97 @@ def processGrips(grips_ToMove, pts_Ref, bDistributeEvenly=False, bEcho=True, bDe
 
         if grip_ToMove.CurrentLocation.DistanceTo(pt_Target) > Rhino.RhinoMath.ZeroTolerance:
             grip_ToMove.CurrentLocation = pt_Target
-            bSomePtTrans = True
+            bSomeGripsWereTranslated = True
 
-    if bDistributeEvenly:
+
+    if not bDistributeEvenly:
+        if not bSomeGripsWereTranslated:
+            print("Grips are already at target locations. No objects were modified.")
+            return
+
+        for rdOwner in rdOwners:
+            sc.doc.Objects.GripUpdate(rdOwner, deleteOriginal=True)
+
+        return
+
+
+
+    # Distribute grips evenly.
+
+
+    def sortGripsFromPt(grips_In, pt):
         fDists_From1stRef = []
-        for grip_ToMove in grips_ToMove:
+        for grip_ToMove in grips_In:
             fDists_From1stRef.append(
-                ptA.DistanceTo(grip_ToMove.CurrentLocation))
+                pt.DistanceTo(grip_ToMove.CurrentLocation))
 
         grips_ToMove_Sorted = []
-        for dist, grip in sorted(zip(fDists_From1stRef, grips_ToMove)):
+        for dist, grip in sorted(zip(fDists_From1stRef, grips_In)):
             grips_ToMove_Sorted.append(grip)
 
-        for i in range(len(grips_ToMove_Sorted)):
-            perunum = float(i)/float(len(grips_ToMove_Sorted)-1.0)
-            pt_Target = ptA + (ptB - ptA) * perunum
-            grip_ToMove = grips_ToMove_Sorted[i]
-            if grip_ToMove.CurrentLocation.DistanceTo(pt_Target) > Rhino.RhinoMath.ZeroTolerance:
-                grip_ToMove.CurrentLocation = pt_Target
-                bSomePtTrans = True
+        return grips_ToMove_Sorted
 
 
-    if not bSomePtTrans:
-        print("Grips are already at target locations.")
+    def evenlyRedistributeSortedGrips(grips, ptStart, ptEnd, bOuterGripsAtExtents):
+        bSomeGripsWereTranslated = False
+        if bOuterGripsAtExtents:
+            for i in range(len(grips)):
+                perunum = float(i) / float(len(grips) - 1)
+                pt_Target = ptStart + (ptEnd - ptStart) * perunum
+                grip_ToMove = grips_ToMove_Sorted[i]
+                if grip_ToMove.CurrentLocation.DistanceTo(pt_Target) > Rhino.RhinoMath.ZeroTolerance:
+                    grip_ToMove.CurrentLocation = pt_Target
+                    bSomeGripsWereTranslated = True
+        else:
+             for i in range(len(grips)):
+                perunum = float(i + 1) / float(len(grips) + 1)
+                pt_Target = ptStart + (ptEnd - ptStart) * perunum
+                grip_ToMove = grips[i]
+                if grip_ToMove.CurrentLocation.DistanceTo(pt_Target) > Rhino.RhinoMath.ZeroTolerance:
+                    grip_ToMove.CurrentLocation = pt_Target
+                    bSomeGripsWereTranslated = True
+
+        return bSomeGripsWereTranslated
+
+
+    if bBetweenRefPts:
+        grips_ToMove_Sorted = sortGripsFromPt(grips_ToMove, ptA)
+
+        bSomeGripsWereTranslated |= evenlyRedistributeSortedGrips(
+            grips_ToMove_Sorted,
+            ptA,
+            ptB,
+            bOuterGripsAtExtents=bOuterGripsAtRefPts
+        )
+    else:
+        # Do not distribute between the 2 reference points.
+        # The 2 points extents (not necessarily the reference points) remain
+        # and the rest are distributed between them.
+        bSuccess, line = rg.Line.TryFitLineToPoints(
+            [grip.CurrentLocation for grip in grips_ToMove])
+        if not bSuccess:
+            print("Line could not be generated from transformed grip locations.")
+        else:
+            grips_ToMove_Sorted = sortGripsFromPt(grips_ToMove, line.From)
+
+            bSomeGripsWereTranslated |= evenlyRedistributeSortedGrips(
+                grips_ToMove_Sorted,
+                line.From,
+                line.To,
+                bOuterGripsAtExtents=True
+            )
+            for i in range(len(grips_ToMove_Sorted)):
+                perunum = float(i) / float(len(grips_ToMove_Sorted) - 1)
+                pt_Target = line.From + (line.To - line.From) * perunum
+                grip_ToMove = grips_ToMove_Sorted[i]
+                if grip_ToMove.CurrentLocation.DistanceTo(pt_Target) > Rhino.RhinoMath.ZeroTolerance:
+                    grip_ToMove.CurrentLocation = pt_Target
+                    bSomeGripsWereTranslated = True
+
+            # print(sc.doc.Objects.AddLine(line))
+
+    if not bSomeGripsWereTranslated:
+        print("Grips are already at target locations. No objects were modified.")
         return
 
     for rdOwner in rdOwners:
@@ -246,24 +350,25 @@ def main():
     grips_ToMove = getInput_Grips_ToMove()
     if grips_ToMove is None: return
 
-    bDistributeEvenly = Opts.values['bDistributeEvenly']
-    bEcho = Opts.values['bEcho']
     bDebug = Opts.values['bDebug']
 
     pts_Ref = getInput_Points_Ref()
     if pts_Ref is None: return
 
     bDistributeEvenly = Opts.values['bDistributeEvenly']
+    bBetweenRefPts = Opts.values['bBetweenRefPts']
+    bOuterGripsAtRefPts = Opts.values['bOuterGripsAtRefPts']
     bEcho = Opts.values['bEcho']
     bDebug = Opts.values['bDebug']
 
-    if not bDebug:
-        sc.doc.Views.RedrawEnabled = False
+    if not bDebug: sc.doc.Views.RedrawEnabled = False
 
     processGrips(
         grips_ToMove,
         pts_Ref,
         bDistributeEvenly=bDistributeEvenly,
+        bBetweenRefPts=bBetweenRefPts,
+        bOuterGripsAtRefPts=bOuterGripsAtRefPts,
         bEcho=bEcho,
         bDebug=bDebug)
 

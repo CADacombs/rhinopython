7e54543eae2cfecfa78322998fedf31609926827
sburzinski <spburzin@gmail.com>
2025-10-10 21:45:38 -0500

    Add files via upload

--- spb_JoinNakedEdges.py
+++ spb_JoinNakedEdges.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 231001: Refactored.  Added text (indices of bad brep components) to dots.
 250215: '+' now is included in reporting of changes in edge counts.
 250916: Modified an option default value.
-251009: WIP: Added an option to join edges iteratively through tolerance values.
+251009-10: Added an option to join edges iteratively through tolerance values.
 """
 
 import Rhino
@@ -25,6 +25,7 @@ import Rhino.Input as ri
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 
+from System import Guid
 from System.Drawing import Color
 
 #import re
@@ -50,7 +51,7 @@ class Opts:
     stickyKeys[key] = '{}({})'.format(key, __file__)
 
     key = 'fStartTol'; keys.append(key)
-    values[key] = 1e-6 * Rhino.RhinoMath.UnitScale(
+    values[key] = 1e-5 * Rhino.RhinoMath.UnitScale(
         Rhino.UnitSystem.Millimeters, sc.doc.ModelUnitSystem)
     riOpts[key] = ri.Custom.OptionDouble(values[key])
     stickyKeys[key] = '{}({})({})'.format(key, __file__, sc.doc.Name)
@@ -238,7 +239,15 @@ def getInput():
                 break
 
 
-def countsOfEdgeValences(rgBrep0):
+def _countsOfEdgeValences(rgBrep0):
+    """
+    From https://developer.rhino3d.com/api/rhinocommon/rhino.geometry.edgeadjacency
+    EdgeAdjacency enum
+    None = 0            Edge is not used by any faces and is therefore superfluous.
+    Naked = 1           Edge is used by a single face.
+    Interior = 2        Edge is used by two adjacent faces.
+    NonManifold = 3     Edge is used by three or more adjacent faces.
+    """
     numEdgeValence = [0]*4
     for e in rgBrep0.Edges:
         numEdgeValence[e.Valence.value__] += 1
@@ -307,7 +316,7 @@ def _dotFace(rgBrep, idxFace):
 
     ptCentroid = amp.Centroid
     getrc, u, v = rgFace.ClosestPoint(ptCentroid)
-    if rg.BrepFace.IsPointOnFace(u, v):
+    if rg.BrepFace.IsPointOnFace(rgFace, u, v):
         location = rgFace.PointAt(u, v)
     else:
         pts = []
@@ -336,10 +345,10 @@ def _dotFace(rgBrep, idxFace):
 def _report_invalid_Brep_after_JoinNakedEdges(brep, bEcho=True, bMarkBadGeom=True):
     if bEcho:
         if bMarkBadGeom:
-            print("Brep became invalid at marked area(s). ")
+            print("Brep became invalid at marked area(s).")
         else:
-            print("New brep geometry is invalid.")
-        sLog = rgBrepX.IsValidWithLog()[1]
+            print("Brep became invalid at latest JoinNakedEdges.")
+        sLog = brep.IsValidWithLog()[1]
         print(sLog)
     if bMarkBadGeom:
         ## Alternative
@@ -349,16 +358,116 @@ def _report_invalid_Brep_after_JoinNakedEdges(brep, bEcho=True, bMarkBadGeom=Tru
             idxF = int(
                     sLog.split('ON_Brep.m_F[', 1)[1].\
                     split('] is invalid.')[0])
-            _dotFace(rgBrepX, idxF)
+            _dotFace(brep, idxF)
         if 'ON_Brep.m_E[' in sLog:
             idxE = int(
                     sLog.split('ON_Brep.m_E[', 1)[1].\
                     split('] is invalid.')[0])
-            _dotEdge(rgBrepX, idxE)
+            _dotEdge(brep, idxE)
 
 
-def processBrepObject(gBrep, fMaxTol=None, fStartTol=None, bMarkBadGeom=True, bEcho=None, bDebug=None):
+def _formatDistance(fDistance):
+    try:
+        fDistance = float(fDistance)
+    except:
+        return "(No deviation provided)"
+
+    if fDistance == 0.0:
+        return "0"
+
+    if fDistance < 10**-(sc.doc.ModelDistanceDisplayPrecision-1):
+        return "{:.4e}".format(fDistance)
+
+    return "{:.{}f}".format(fDistance, sc.doc.ModelDistanceDisplayPrecision)
+
+
+def _formatDelta(integer):
+    sign_char = '+' if integer > 0 else '' # Empty string for zero and negative
+    return "{0:{sign}d}".format(integer, sign=sign_char)
+
+
+def processBrep(rgBrep_In, fMaxTol, fStartTol, bMarkBadGeom=True, bEcho=True, bDebug=False):
     """
+    """
+
+    if rgBrep_In.IsSolid:
+        if bEcho:
+            print("Brep is already closed.")
+        return
+
+    if not rgBrep_In.IsValid:
+        if bEcho:
+            print("Starting Brep is invalid and will not be processed.")
+        return
+
+    rgBrep_PrevWIP = None
+    rgBrep_CurrentWIP = rgBrep_In.DuplicateBrep()
+    fTol_Current = fStartTol
+    ct_JoinedEdges_Total = 0
+
+    Rhino.RhinoApp.SetCommandPrompt(prompt="Joining naked edges ...")
+
+    while True:
+        sc.escape_test()
+
+        iJoinCt = rgBrep_CurrentWIP.JoinNakedEdges(fTol_Current)
+
+        if bEcho and rgBrep_CurrentWIP.IsValid:
+            print("{} edges were joined JoinNakedEdges(tolerance={}).".format(
+                iJoinCt,
+                _formatDistance(fTol_Current)))
+
+        if not rgBrep_CurrentWIP.IsValid and (bEcho or bMarkBadGeom):
+            _report_invalid_Brep_after_JoinNakedEdges(rgBrep_CurrentWIP, bEcho, bMarkBadGeom)
+            rgBrep_CurrentWIP.Dispose()
+            print("{} edges were joined JoinNakedEdges(tolerance={:f}) to an invalid brep.".format(
+                iJoinCt,
+                fTol_Current))
+            if ct_JoinedEdges_Total:
+                return rgBrep_PrevWIP # Will be None if no JoinNakedEdges were successful and didn't create an invalid brep.
+            else:
+                rgBrep_PrevWIP.Dispose()
+                return
+
+        ct_JoinedEdges_Total += iJoinCt
+
+        if rgBrep_PrevWIP:
+            rgBrep_PrevWIP.Dispose()
+
+        if fTol_Current == fMaxTol:
+            break
+
+        rgBrep_PrevWIP = rgBrep_CurrentWIP
+        rgBrep_CurrentWIP = rgBrep_CurrentWIP.DuplicateBrep()
+
+        fTol_Current *= 10.0
+        #sEval = "fMaxTol, fTol_Current, fMaxTol - fTol_Current"; print(sEval,'=',eval(sEval))
+        #sEval = "abs(fMaxTol - fTol_Current)/max((fMaxTol, fTol_Current))"; print(sEval,'=',eval(sEval))
+        if fTol_Current > fMaxTol:
+            fTol_Current = fMaxTol
+        elif ((fMaxTol - fTol_Current) / fMaxTol) <= 0.1:
+            fTol_Current = fMaxTol
+
+    if ct_JoinedEdges_Total == 0:
+        rgBrep_CurrentWIP.Dispose()
+        return
+
+    return rgBrep_CurrentWIP
+
+
+def coerceBrepObject(rhBrep_In):
+    if isinstance(rhBrep_In, rd.BrepObject):
+        return rhBrep_In
+    elif isinstance(rhBrep_In, Guid):
+        return sc.doc.Objects.FindId(rhBrep_In)
+    else:
+        raise Exception("{} passed to function. Should be BrepObject or GUID of one).".format(
+            rdBrep_In.GetType().Name))
+
+
+def processBrepObject(rhBrep_In, fMaxTol=None, fStartTol=None, bMarkBadGeom=True, bEcho=None, bDebug=None):
+    """
+    rhBrep_In: BrepObject or GUID of one
     """
 
     if fMaxTol is None: fMaxTol = Opts.values['fMaxTol']
@@ -367,85 +476,77 @@ def processBrepObject(gBrep, fMaxTol=None, fStartTol=None, bMarkBadGeom=True, bE
     if bEcho is None: bEcho = Opts.values['bEcho']
     if bDebug is None: bDebug = Opts.values['bDebug']
 
-    rgBrepX = rs.coercebrep(gBrep)
 
-    if rgBrepX.IsSolid:
-        if bEcho:
-            print("Brep is already closed.")
-        return
+    rdBrep_In = coerceBrepObject(rhBrep_In)
+    rgBrep_In = rdBrep_In.BrepGeometry
 
-    if not rgBrepX.IsValid:
-        if bEcho:
-            print("Starting Brep is invalid and will not be processed.")
+    rgBrep_Res = processBrep(
+        rgBrep_In,
+        fMaxTol=fMaxTol,
+        fStartTol=fStartTol,
+        bMarkBadGeom=bMarkBadGeom,
+        bEcho=bEcho,
+        bDebug=bDebug,
+        )
+
+    if rgBrep_Res is None:
         return
 
     # Report face count and edge valence counts.
-    numF_B0 = rgBrepX.Faces.Count
-    numEValence_B0 = countsOfEdgeValences(rgBrepX)
-    
-    print("Counts: {} faces, {} none edges, {} naked edges,"
-          "{} interior edges, {} non-manifold edges".format(
-              numF_B0,
-              numEValence_B0[0],
-              numEValence_B0[1],
-              numEValence_B0[2],
-              numEValence_B0[3],
-              ))
+    ct_Faces_In = rgBrep_In.Faces.Count
+    cts_of_EdgeValences_In = _countsOfEdgeValences(rgBrep_In)
 
-    Rhino.RhinoApp.SetCommandPrompt(prompt="Joining naked edges ...")
-    
-    fTol_Current = fStartTol
-    iJoinCt_Total = 0
+    print("Counts:",
+          "{} faces,".format(ct_Faces_In),
+          "{} none edges,".format(cts_of_EdgeValences_In[0]),
+          "{} naked edges,".format(cts_of_EdgeValences_In[1]),
+          "{} interior edges,".format(cts_of_EdgeValences_In[2]),
+          "{} non-manifold edges".format(cts_of_EdgeValences_In[3]))
 
-    while fTol_Current <= fMaxTol:
-        iJoinCt = rgBrepX.JoinNakedEdges(fTol_Current)
+    if not sc.doc.Objects.Replace(rdBrep_In.Id, rgBrep_Res):
+        print("Brep was not replaced.")
+        return False
 
-        if bEcho and iJoinCt:
-            print("{} edges were joined within a deviation of {:f}.".format(fTol_Current))
+    sc.doc.Views.Redraw()
+    ct_Faces_Res = rgBrep_Res.Faces.Count
+    cts_of_EdgeValences_Res = _countsOfEdgeValences(rgBrep_Res)
 
-        if not rgBrepX.IsValid and (bEcho or bMarkBadGeom):
-            _report_invalid_Brep_after_JoinNakedEdges(rgBrepX, bEcho, bMarkBadGeom)
-            return
+    print("Counts: Before -> After (Delta)")
 
-        iJoinCt_Total += iJoinCt
+    if ct_Faces_In != ct_Faces_Res:
+        print("Faces: {} - {} ({})".format(
+            ct_Faces_In,
+            ct_Faces_Res,
+            _formatDelta(ct_Faces_Res-ct_Faces_In),
+            ))
 
+    if cts_of_EdgeValences_In[0] or cts_of_EdgeValences_Res[0]:
+        print("None edges: {} -> {} ({})".format(
+            cts_of_EdgeValences_In[0],
+            cts_of_EdgeValences_Res[0],
+            _formatDelta(cts_of_EdgeValences_Res[0]-cts_of_EdgeValences_In[0]),
+            ))
 
-        if not sc.doc.Objects.Replace(gBrep, rgBrepX):
-            print("Brep was not replaced.")
-            return False
-
-        sc.doc.Views.Redraw()
-        numF_B1 = rgBrepX.Faces.Count
-        numEValence_B1 = countsOfEdgeValences(rgBrepX)
-        
-        print("Counts: Before -> After = Change")
-        print("Faces: {} - {} = {:+}".format(
-                numF_B0,
-                numF_B1,
-                numF_B1-numF_B0,
+    print("Naked edges: {} -> {} ({})".format(
+        cts_of_EdgeValences_In[1],
+        cts_of_EdgeValences_Res[1],
+        _formatDelta(cts_of_EdgeValences_Res[1]-cts_of_EdgeValences_In[1]),
         ))
-        print("None edges: {} -> {} = {:+}".format(
-                numEValence_B0[0],
-                numEValence_B1[0],
-                numEValence_B1[0]-numEValence_B0[0],
+
+    print("Interior edges: {} -> {} ({})".format(
+        cts_of_EdgeValences_In[2],
+        cts_of_EdgeValences_Res[2],
+        _formatDelta(cts_of_EdgeValences_Res[2]-cts_of_EdgeValences_In[2]),
         ))
-        print("Naked edges: {} -> {} = {:+}".format(
-                numEValence_B0[1],
-                numEValence_B1[1],
-                numEValence_B1[1]-numEValence_B0[1],
-        ))
-        print("Interior edges: {} -> {} = {:+}".format(
-                numEValence_B0[2],
-                numEValence_B1[2],
-                numEValence_B1[2]-numEValence_B0[2],
-        ))
-        print("Non-manifold edges: {} -> {} = {:+}".format(
-                numEValence_B0[3],
-                numEValence_B1[3],
-                numEValence_B1[3]-numEValence_B0[3],
-        ))
-        
-        return rgBrepX
+
+    if cts_of_EdgeValences_In[3] or cts_of_EdgeValences_Res[3]:
+        print("Non-manifold edges: {} -> {} ({})".format(
+            cts_of_EdgeValences_In[3],
+            cts_of_EdgeValences_Res[3],
+            _formatDelta(cts_of_EdgeValences_Res[3]-cts_of_EdgeValences_In[3]),
+            ))
+
+    return rdBrep_In
 
 
     #print("No edges were joined within a deviation of {:f}.".format(fMaxTol))
